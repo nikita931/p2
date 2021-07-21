e55e22b257ad3ff85473aad0bf46243f35f48dae
nikita931 <65214885+nikita931@users.noreply.github.com>
2021-07-21 12:41:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© 2021. Made by Nikita Chavan <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('nav.php')?>
+
+
+
+
+     <h1><b><i>Welcome to Sparks Bank</i></b></h1>
+
+  <div class="bg">
+     
+         <ul>
+		     <section>
+	              &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+	              <img src="img/logo1.png" height="200" width="200" class="im">
+	               &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp
+	               <img src="img/transfer_money.jpg" height="200" width="200" class="im">
+	               &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp
+	               <img src="img/history.png" height="200" width="200" class="im">
+            
+               </section>
+              &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <a href="transfer_money.php"><button class="btn" id="red">View All Users</button></a></li>
+             &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <a href="transfer_money.php"><button class="btn" id="blue">Trasnfer Money</button></a></li>
+			 &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp <a href="transactionhistory.php"><button class="btn" id="green">View Transfer History</button></a></li>
+			  
+          </ul>
+     
+  </div>
+ <?php include('footer.php') ?>
+  
+</body>
+</html>
